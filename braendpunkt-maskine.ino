8bc7b48c7e45dd7e0894ff7b39d6a474ff4ce24f
Gibbus2 <gibbusf@gmail.com>
2018-11-23 15:40:48 +0100

    motor now dependent on sensors

--- braendpunkt-maskine.ino
+++ braendpunkt-maskine.ino
@@ -1,24 +1,59 @@
 int venstre = A1;
 int hojre = A0;
 
-int motorMedUret = 6;
-int motorModUret = 5;
+int motorHojre = 8;
+int motorVenstre = 7;
+
+int motorOp = 12;
+int motorNed = 13;
 
 void setup() {
-  // put your setup code here, to run once:
+  // Sensor setup
   pinMode(venstre, INPUT);
   pinMode(hojre, INPUT);
-  pinMode(motorMedUret, OUTPUT);
-  pinMode(motorModUret, OUTPUT);
+
+  // Motor pin setup
+  pinMode(motorHojre, OUTPUT);
+  pinMode(motorVenstre, OUTPUT);
+  pinMode(motorOp, OUTPUT);
+  pinMode(motorNed, OUTPUT);
+
+  /*
+  digitalWrite(motorOp, LOW);
+  digitalWrite(motorNed, LOW);
+  digitalWrite(motorOp, LOW);
+  digitalWrite(motorNed, LOW);  */
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  int ldrV = analogRead(venstre);
+  int ldrH = analogRead(hojre);
+
+  
+  if (ldrV > ldrH) {
+    digitalWrite(motorVenstre, HIGH);
+    digitalWrite(motorHojre, LOW);
+    Serial.println("Drejer mod uret: LDR venstre = " + String(ldrV));
+  } else if (ldrH > ldrV) {
+    digitalWrite(motorHojre, HIGH);
+    digitalWrite(motorVenstre, LOW);
+    Serial.println("Drejer med uret: LDR hojre = " + String(ldrH));
+  } else {
+    Serial.println("Equalized light absorbsion");
+  }
+  
+
+  delay(500);
+  
+  digitalWrite(motorVenstre, LOW);
+  digitalWrite(motorHojre, LOW);
+
+  delay(3000);
+
+  /*
   Serial.println("Venstre: " + String(analogRead(venstre)));
   Serial.println("Højre: " + String(analogRead(hojre)));
-  analogWrite(motorModUret, 1023);
-  delay(300);
-  analogWrite(motorModUret, 0);
-  delay(1500);
+  */
 }

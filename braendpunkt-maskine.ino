fb9e653cdad291094611df4f727f051b33104817
Maastrup <karljohan@maastrup.dk>
2018-11-30 14:39:11 +0100

    Added angle motor + commented and cleaned up code

--- braendpunkt-maskine.ino
+++ braendpunkt-maskine.ino
@@ -1,59 +1,85 @@
-int venstre = A0;
-int hojre = A1;
+// Definerer input pins til LDR sensorer
+#define venstreSens A0;
+#define hojreSens A1;
+#define opSens A2;
+#define nedSens A3;
 
-int motorHojre = 6;
-int motorVenstre = 5;
-
-int motorOp = 10;
-int motorNed = 11;
+// Definerer motor output pins til aktivering
+#define motorHojre 6;
+#define motorVenstre 5;
+#define motorOp 10;
+#define motorNed 11;
 
 
 void setup() {
-  // Sensor setup
-  pinMode(venstre, INPUT);
-  pinMode(hojre, INPUT);
+  // Sensor pin setup
+  pinMode(venstreSens, INPUT);
+  pinMode(hojreSens, INPUT);
+  pinMode(opSens, INPUT);
+  pinMode(nedSens, INPUT);
 
   // Motor pin setup
   pinMode(motorHojre, OUTPUT);
   pinMode(motorVenstre, OUTPUT);
   pinMode(motorOp, OUTPUT);
   pinMode(motorNed, OUTPUT);
-  /*
-  digitalWrite(motorOp, LOW);
-  digitalWrite(motorNed, LOW);
-  digitalWrite(motorOp, LOW);
-  digitalWrite(motorNed, LOW);  */
+
+  // Starter seriel kommunikation på serielport 9600
   Serial.begin(9600);
 }
 
 void loop() {
-  
-  int ldrV = analogRead(venstre);
-  int ldrH = analogRead(hojre);
+  //input fra LDR
+  int vVal = analogRead(venstreSens);
+  int hVal = analogRead(hojreSens);
+  int opVal = analogRead(opSens);
+  int nedVal = analogRead(nedSens);
 
-  int dLight = ldrV - ldrH; 
+  //forskel på LDR
+  int dLys_vandret = vVal - hVal;
+  int dLys_lodret = opVal - nedVal;
 
-  int powerOut = map(absolut(dLight), 0, 200, 0, 512);
-  Serial.println("Diff: " + String(dLight));
-  Serial.println("Venstre: " + String(ldrV));
-  Serial.println("Hojre: " + String(ldrH));
+/*   Serial.println("Diff: " + String(dLys_vandret));
+  Serial.println("Venstre: " + String(vVal));
+  Serial.println("Hojre: " + String(hVal)); */
   
-  if (ldrV > ldrH) {
+  // Vandret korrektion
+  int powerOut = map(absolut(dLys_vandret), 0, 200, 0, 512);
+
+  if (vVal > hVal) {
     analogWrite(motorVenstre, powerOut);
     digitalWrite(motorHojre, LOW);
-    Serial.println("Drejer mod uret: LDR venstre = " + String(ldrV));
-  } else if (ldrH > ldrV) {
+    Serial.println("Drejer mod uret: LDR venstre = " + String(vVal));
+  } else if (hVal > vVal) {
     analogWrite(motorHojre, powerOut);
     digitalWrite(motorVenstre, LOW);
-    Serial.println("Drejer med uret: LDR hojre = " + String(ldrH));
+    Serial.println("Drejer med uret: LDR hojre = " + String(hVal));
+  } else {
+    Serial.println("Equalized light absorbsion");
+  }
+  Serial.println("Power output = " + String(powerOut));
+
+  // Vinkel korrektion
+  powerOut =  map(absolut(dLys_lodret), 0, 200, 0, 1023);
+
+  if (opVal > nedVal) {
+    analogWrite(motorOp, powerOut);
+    digitalWrite(motorNed, LOW);
+    Serial.println("Forstørrer linse vinkel: LDR op = " + String(opVal));
+  } else if (nedVal > opVal) {
+    analogWrite(motorNed, powerOut);
+    digitalWrite(motorOp, LOW);
+    Serial.println("Mindsker linse vinkel: LDR ned = " + String(nedVal));
   } else {
     Serial.println("Equalized light absorbsion");
   }
   
   Serial.println("Power output = " + String(powerOut));
+  
   delay(10);
 }
 
+//ændre val til positiv (hvis val er negativ)
 int absolut(int val){
   if(val < 0){
     val *= -1;
